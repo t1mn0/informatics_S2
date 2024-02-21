5a79537d0086eb38bb96ed87bc15346f429bc2f9
t1mn0 <timofeyalbecov@gmail.com>
2024-02-21 16:36:17 +0300

    update tests

--- lab1/lab1/main.c
+++ lab1/lab1/main.c
@@ -17,7 +17,7 @@ char* testMapStringFunc(char* str) {
 
 int testWhereFunc(char* str) {
     for (int i = 0; i < strlen(str); i++) {
-        if (str[i] == 't') {
+        if (str[i] == 'w') {
             return 1;
         }
     }
@@ -32,18 +32,32 @@ int main() {
     dynamic_array* darr2 = newList();
 
     addDouble(darr1, 3.1415);
-    addString(darr1, "code");
+    addString(darr1, "c++");
     addDouble(darr1, 12);
     addString(darr1, "github");
     addDouble(darr1, 10);
     addString(darr1, "auto");
+    addDouble(darr1, 16);
+    addString(darr1, "code");
+    addDouble(darr1, 25.5);
+    addString(darr1, "classic");
+    addDouble(darr1, 0);
+    addString(darr1, "snake");
+    addDouble(darr1, -144);
+    addString(darr1, "iron");
+    addDouble(darr1, -1000);
+    addString(darr1, "gitlab");
+    addDouble(darr1, -0.001);
+    addString(darr1, "long float");
 
     addDouble(darr2, 35981);
     addString(darr2, "world");
     addDouble(darr2, -43.43);
     addString(darr2, "start");
     addDouble(darr2, 0);
-    addString(darr2, "task");
+    addString(darr2, "qwerty");
+    addString(darr2, "wasd");
+    addString(darr2, "super");
 
 
     printf("- - - - - - - - - - - - - - - - - - - - - - - - - - \n");
@@ -80,14 +94,14 @@ int main() {
     printList(darr2);
     printf("- - - - - - - - - - - - - - - - - - - - - - - - - - \n");
 
-    printf("%sWhere ('t' in the string) of list_2%s\n", PURPLE, RESET);
+    printf("%sWhere ('w' in the string) of list_2%s\n", PURPLE, RESET);
     darr2 = where(testWhereFunc, darr2);
     printf("- - - - - - - - - - - - - - - - - - - - - - - - - - \n");
     printf("%slist_2:%s\n", PURPLE, RESET);
     printList(darr2);
     printf("- - - - - - - - - - - - - - - - - - - - - - - - - - \n");
 
-    printf("%sConcatenation of list_1 and list_2\n", PURPLE, RESET);
+    printf("%sConcatenation of list_1 and list_2%s\n", PURPLE, RESET);
     dynamic_array* darr = concatenation(darr1, darr2);
 
     printf("- - - - - - - - - - - - - - - - - - - - - - - - - - \n");
